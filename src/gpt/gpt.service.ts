119559f2b8731eb9c14bdd49d19e6ef581c6d421
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2024-05-20 13:07:28 -0500

    ✨ Feat: 👷🏽 - First OpenApi prompt

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { orthographyCheckUseCase } from './use-cases';
 import { OrthographyDto } from './dto';
+import OpenAI from 'openai';
 
 @Injectable()
 export class GptService {
   // solo va a llamar casos de uso
 
+  private openAi = new OpenAI({
+    apiKey: process.env.OPEN_API_KEY,
+  });
+
   async orthographyCheck(orthographyDto: OrthographyDto) {
-    return await orthographyCheckUseCase({
+    return await orthographyCheckUseCase(this.openAi, {
       prompt: orthographyDto.prompt,
     });
   }
--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -1,12 +1,26 @@
+import OpenAI from 'openai';
+
 interface Options {
   prompt: string;
 }
 
-export const orthographyCheckUseCase = async (options: Options) => {
+export const orthographyCheckUseCase = async (
+  openai: OpenAI,
+  options: Options,
+) => {
   const { prompt } = options;
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: 'system',
+        content:
+          'Tu nombre es Gon, debes responder amablemente siempre y dar tu nombre.',
+      },
+      { role: 'user', content: prompt },
+    ],
+    model: 'gpt-4o',
+  });
 
-  return {
-    prompt: prompt,
-    apiKay: process.env.OPEN_API_KEY,
-  };
+  console.log(completion);
+  return completion.choices[0];
 };
