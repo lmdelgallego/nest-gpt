a23a41e0d84f8a75defa6c6e32ec78c210c4b4af
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2024-05-20 11:24:43 -0500

    ✨ Feat: 👷🏽 - Send Dto's params to useCases

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { orthographyCheckUseCase } from './use-cases';
+import { OrthographyDto } from './dto';
 
 @Injectable()
 export class GptService {
   // solo va a llamar casos de uso
 
-  async orthographyCheck() {
-    return await orthographyCheckUseCase();
+  async orthographyCheck(orthographyDto: OrthographyDto) {
+    return await orthographyCheckUseCase({
+      prompt: orthographyDto.prompt,
+    });
   }
 }
